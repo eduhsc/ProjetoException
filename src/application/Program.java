0174dc73516f0ff720eb9d1552bfa3d3f59aca9a
Eduardo Haddad <eduardochaves51@gmail.com>
2019-08-18 09:17:04 -0300

    Alterações Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 import model.entities.Account;
+import model.exceptions.DomainException;
 
 public class Program {
 
@@ -12,6 +14,7 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+	try {
 		System.out.println("Enter account data:");
 		System.out.print("Number: ");
 		int number = sc.nextInt();
@@ -23,7 +26,21 @@ public class Program {
 		double limit = sc.nextDouble();
 		Account acc = new Account(number, holder, balance, limit);
 		
-		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		acc.withDraw(sc.nextDouble());
+		System.out.println("New balance: " + String.format("%.2f",acc.getBalance()));
+	}
+	catch (DomainException e) {
+			System.out.println(e.getMessage());
+		}
+	catch (InputMismatchException e) {
+		System.out.println("ERROR:Dados inválidos!");
+	}
+	catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 		sc.close();
 
 	}
